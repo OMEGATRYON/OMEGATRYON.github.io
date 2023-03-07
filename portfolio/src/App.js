a41854e67dd33dbbb48ffa2c1cc09d7a03aff6ef
OMEGATRYON <tryon.baldwin@student.awesomeinc.org>
2023-03-07 21:05:43 +0000

    converted blog project over to react

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,13 @@
-
+import Boutme from "./components/Boutme";
+import Header from "./components/Header";
+import BasicExample from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
-    hello world!
+      <BasicExample />
+      <Header />
+      <Boutme />
     </div>
   );
 }
