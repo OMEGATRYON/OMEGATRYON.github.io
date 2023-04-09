e785fb0a6ba5293b5b33319b6affc5a72a686267
OMEGATRYON <tryon.baldwin@student.awesomeinc.org>
2023-04-09 20:18:51 +0000

    picture/border complete

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -4,6 +4,12 @@ const Home = () => {
   return (
     <div className="circle-photo-container">
     <img src="/images/header.jpg" alt="header of me" className="circle-photo" />
+    <div className="description">
+        <p className='tryhead'>Tryon Baldwin</p>
+        <p>Full Stack Web Developer</p>
+        <p className='langbio'>JavaScript, React, Bootstrap, Python, Django </p>
+        <p className='langtwo'>object-oriented programming, agile methodologies, and PostgresSQL.</p>
+      </div>
   </div>
   )
 }
